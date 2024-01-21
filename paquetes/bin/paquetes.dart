f711cf91bc8ec27ec0b36c7740dae1bafa20f437
Luis A Morla Sánchez <luislabertmsanchez@gmail.com>
2024-01-20 21:36:03 -0400

    Mapeando respuesta de un Req a una instancia de clase

--- paquetes/bin/paquetes.dart
+++ paquetes/bin/paquetes.dart
@@ -1,5 +1,36 @@
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 import 'package:paquetes/paquetes.dart' as paquetes;
 
+
 void main(List<String> arguments) {
-  print('Hello world: ${paquetes.calculate()}!');
+  
+
+final url =  Uri.parse('https://reqres.in/api/users?page=2');
+
+// var url =
+//       Uri.https('www.googleapis.com', '/books/v1/volumes', {'q': '{http}'});
+
+http.get(url).then( (res) {
+
+
+  print(res);
+
+  final body = jsonDecode(res.body);
+
+  print(body);
+
+  print('page: ${body['page']}');
+
+  print('per_page: ${body['per_page']}');
+
+  print('id del tercer elemento: ${body['data'][2]['id']}');
+
+
+});
+
+
 }
